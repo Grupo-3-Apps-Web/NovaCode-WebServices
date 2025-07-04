be1e782c285e6ac1367400ccff522d524e00cdd8
JosuePaiva02 <u202119095@upc.edu.pe>
2025-07-03 21:02:44 -0500

    fix:  iam null return

--- NovaCode Web Services/IAM/Infrastructure/Pipeline/Middleware/Components/RequestAuthorizationMiddleware.cs
+++ NovaCode Web Services/IAM/Infrastructure/Pipeline/Middleware/Components/RequestAuthorizationMiddleware.cs
@@ -4,8 +4,9 @@ using NovaCode_Web_Services.IAM.Domain.Services;
 using NovaCode_Web_Services.IAM.Infrastructure.Pipeline.Middleware.Attributes;
 
 namespace NovaCode_Web_Services.IAM.Infrastructure.Pipeline.Middleware.Components;
+
 /// <summary>
-/// Request authorization middleware 
+/// Request authorization middleware
 /// </summary>
 /// <param name="next">
 /// <see cref="RequestDelegate"/> Next middleware in pipeline
@@ -13,7 +14,7 @@ namespace NovaCode_Web_Services.IAM.Infrastructure.Pipeline.Middleware.Component
 public class RequestAuthorizationMiddleware(RequestDelegate next)
 {
     /// <summary>
-    /// Invoke middleware 
+    /// Invoke middleware
     /// </summary>
     /// <remarks>
     /// This middleware is responsible for authorizing requests. It checks if the request is allowed to be anonymous. If it is, it skips authorization. If it is not, it validates the token in the request header and retrieves the user associated with the token. It then updates the context with the user and continues to the next middleware in the pipeline.
@@ -36,9 +37,9 @@ public class RequestAuthorizationMiddleware(RequestDelegate next)
         ITokenService tokenService)
     {
         Console.WriteLine("Entering InvokeAsync");
-        var allowAnonymous = context.Request.HttpContext.GetEndpoint()!
-            .Metadata
-            .Any(m => m.GetType() == typeof(AllowAnonymousAttribute));
+        var endpoint = context.Request.HttpContext.GetEndpoint();
+        var allowAnonymous = endpoint != null &&
+            endpoint.Metadata.Any(m => m.GetType() == typeof(AllowAnonymousAttribute));
         Console.WriteLine($"AllowAnonymous: {allowAnonymous}");
         if (allowAnonymous)
         {
@@ -50,11 +51,11 @@ public class RequestAuthorizationMiddleware(RequestDelegate next)
         var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
         if (token is null) throw new Exception("Null of invalid token");
-        
+
         var userId = await tokenService.ValidateToken(token);
-        
+
         if (userId is null) throw new Exception("Invalid token");
-        
+
         var getUserByIdQuery = new GetUserByIdQuery(userId.Value);
 
         var user = await userQueryService.Handle(getUserByIdQuery);
